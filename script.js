d33e04fb292dbbbeb31640a75b5ce20b5638e415
ridhoajibx <ridhoajibx@gmail.com>
2021-07-24 01:56:44 +0700

    [ridho]- refactor index - WIP

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let body = document.querySelector("#dark");
-let header = document.querySelector("header");
-let footer = document.querySelector(".footer");
-let moon = document.querySelector(".icon-moon");
-let sun = document.querySelector(".icon-sun");
-let btnToggle = document.querySelector("#btn-dark");
+// let body = document.querySelector("#dark");
+// let header = document.querySelector("header");
+// let footer = document.querySelector(".footer");
+// let moon = document.querySelector(".icon-moon");
+// let sun = document.querySelector(".icon-sun");
+// let btnToggle = document.querySelector("#btn-dark");
 
-btnToggle.addEventListener("click", handleToggleBtn);
+// function handleToggleBtn (){
+//     if(moon.style.display === "block" && sun.style.display === "none"){
+//         moon.style.display = "none";
+//         sun.style.display = "block";
+//     } else {
+//         moon.style.display = "block";
+//         sun.style.display = "none";
+//     }
+//     body.classList.toggle("dark-mode");
+//     header.classList.toggle("dark-mode");
+//     footer.classList.toggle("dark-mode");
+// }
 
-function handleToggleBtn (){
-    if(moon.style.display === "block" && sun.style.display === "none"){
-        moon.style.display = "none";
-        sun.style.display = "block";
-    } else {
-        moon.style.display = "block";
-        sun.style.display = "none";
-    }
-    body.classList.toggle("dark-mode");
-    header.classList.toggle("dark-mode");
-    footer.classList.toggle("dark-mode");
+let humberger = document.querySelector('#humberger');
+let nav = document.querySelector('.header-right');
+
+humberger.addEventListener("click", handleOpen);
+
+function handleOpen (){
+    nav.classList.toggle("active");
 }
-
